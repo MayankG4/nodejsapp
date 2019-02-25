c9a6e38c9a4538df02f2ac3a9afb7bb23fc553a7
mayankg4 <mayankgupta0589@gmail.com>
2019-02-25 10:58:16 +0530

    add index file

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>Company Name</title>
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/globl.css" media="screen" />
+	<link href="https://fonts.googleapis.com/css?family=Acme|Dosis" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/style.css" media="screen" />
+	<script type="text/javascript" src="assets/js/jquery-1.11.2.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="engine0/style.css" />
+	<link rel="stylesheet" type="text/css" href="engine0/style.css" />
+</head>
+
+<body class="overlap-nav-ltr">
+<?php include('header.php');?>
+<?php 
+include('config.php');
+if(isset($_POST['submit'])){
+echo "submit"
+print_r($_POST);die;
+}
+
+
+?>
+		<!--Content Area Start-->
+			<div class="content">
+				<div class="imagewrapper">
+					<img src="assets/images/ban.png">
+				</div>
+        <div class="container">
+					<div class="maincontent clearfix">
+				<div class="news">
+					<h2>Latest news</h2>
+					<div class="info topic-1">
+						<div class="news-image">
+							<img src="assets/images/rank_images.jpg" alt="club">
+						</div>
+						<div class="news-details">
+						<h5>Recruiting CS:Go Players</h5>
+						<p>Very Soon Atomic Dudes Going to start a CS:Go just like CS 1.6.
+						</p>
+							<div class="button">
+								<a class="moreinfo" href="#">more info</a>
+							</div>
+								<span class=""datepost><i class="fa fa-calendar" aria-hidden="true"></i>25th December,2017</span>
+						</div>
+					</div>
+					<div class="info topic-1">
+						<div class="news-image">
+							<img src="assets/images/rank_images.jpg" alt="club">
+						</div>
+						<div class="news-details">
+						<h5>Recruiting CS:Go Players</h5>
+						<p>Very Soon Atomic Dudes Going to start a CS:Go just like CS 1.6.
+						</p>
+							<div class="button">
+								<a class="moreinfo" href="#">more info</a>
+							</div>
+								<span class=""datepost><i class="fa fa-calendar" aria-hidden="true"></i>25th December,2017</span>
+						</div>
+					</div><div class="info topic-1">
+						<div class="news-image">
+							<img src="assets/images/rank_images.jpg" alt="club">
+						</div>
+						<div class="news-details">
+						<h5>Recruiting CS:Go Players</h5>
+						<p>Very Soon Atomic Dudes Going to start a CS:Go just like CS 1.6.
+						</p>
+							<div class="button">
+								<a class="moreinfo" href="#">more info</a>
+							</div>
+								<span class=""datepost><i class="fa fa-calendar" aria-hidden="true"></i>25th December,2017</span>
+						</div>
+					</div><div class="info topic-1">
+						<div class="news-image">
+							<img src="assets/images/rank_images.jpg" alt="club">
+						</div>
+						<div class="news-details">
+						<h5>Recruiting CS:Go Players</h5>
+						<p>Very Soon Atomic Dudes Going to start a CS:Go just like CS 1.6.
+						</p>
+							<div class="button">
+								<a class="moreinfo" href="#">more info</a>
+							</div>
+								<span class=""datepost><i class="fa fa-calendar" aria-hidden="true"></i>25th December,2017</span>
+						</div>
+					</div>
+		</div>
+				<div class="match-section">
+					<h2>Upcoming Match</h2>
+				<div class="nextmatch">
+					<span class="teamname">Miracle Fan Boy</span>
+					<span class="vs">vs</span>
+					<span class="teamnames">Excuse</span>
+					<div class="button">
+						<a class="moreinfo" href="#">more info</a>
+					</div>
+				</div>
+		</div>
+	<div class="registernick">
+		<h2>Register your nick</h2>
+		<div class="form-wrapper">
+		<form action="" method='post' style="border:1px solid #ccc">
+			<label><b>Email</b></label>
+			<input type="text" placeholder="Enter Email" name="email" required>
+
+			<label><b>Nick</b></label>
+			<input type="text" placeholder="Enter your nick" name="nick" required>
+
+			<label><b>Password</b></label>
+			<input type="password" placeholder="Enter Password" name="psw" required>
+
+			<label><b>Repeat Password</b></label>
+			<input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+			<input type="checkbox" checked="checked"> Remember me
+			<p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+
+			<div class="button-wrapper">
+			  <button class="cancel" type="button" class="cancelbtn">Cancel</button>
+			  <button class="submission" type="submit" name=submit class="signupbtn">Sign Up</button>
+			</div>			
+        </form>
+		</div>
+	</div>
+</div>
+<div class="performer">
+		<div class="bestplayer">
+			<h2>Player of the month</h2>
+		<div class="playerofmonth">
+			<div class="players-profile front">
+				<div class="player-image">
+					<img src="assets/images/lone.png">
+				</div>
+				<span class="player-name">Lone Ranger</span>
+			<div class="players-back">
+				<span class="weapon clearfix"><span class="name">Weapon</span><span class="attribute"><img src="assets/images/w_awp.png"></span></span>
+				<span class="fav-map clearfix"><span class="name">Map</span><span class="attribute">Dust 2</span></span>
+				<span class="totalkills clearfix"><span class="name">Score</span><span class="attribute">total kills</span></span>
+				<p>I am glad that I am member of Atomic Dudes. Being a member here I have learnt a lot even explore my experience too. As Atomic Dudes a gaming group have learnt a lot of game plays which I have never experienced before.</p>
+				<p>The  best thing which I have ever enjoyed about is talking getting to know each other and also gain a Band Of Brothers.</p> <p>Lastly, thanks a lot to my founder of Atomic Dudes and whole management team.</p>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+	<div class="roster">
+		<div class="management-team">
+			<div class="player-wrapper">
+				<div class="player-image">
+					<img src="assets/images/miracle.png">
+				</div>
+				<div class="player-details">
+					<span class="player-name">Miracle fan boy</span>
+					<span class="player-designation">
+						founder
+					</span>
+					<span class="social-links">safaf</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<div class="ourstory">
+	<div class="container">
+<h2>About Us</h2>
+		<p>Atomic Dudes was founded on 4 September, 2017 by a goup of crazy gamers who are still addicted and love playing CS 1.6 when the entire counter strike community has moved on to CS GO. Our mission is to "provide a place to all gamers of all calibers form across the world to enjoy the game and be part of AD family".
+Our servers are running 24*7 managed by our server managers along with the co-founder and founder who are one of the most dedicated and experienced in the gaming world. We strongly beleive in unity, loyalty and integrity. We play as a family helping others not only improve their game play but also provide a competitive gaming experience. What was to come was a transformation of a small emerging clan of 4-5 members into a family of 40+ members from all the parts of the world.</p>
+<p>We organize monthly tournaments and weekly challenges where in all the clubs of the AD community compete against each other. Top team and best player are given free excting gifts. So come be part of us and enjoy the Game.</p>
+</div>
+</div>
+</div>
+
+			<!--Content Area End-->
+			<!--Footer Section Start-->
+			<div class="footer">
+				<div class="container clearfix">
+					<div class="foot col-1">
+						<h6>Latest posts</h6>
+						<ul class="post">
+							<li><span class="posttitle">CS Go server Launcher
+							</span><p> Very Soon, Atomic Dudes is going to host a CS Go server.</p></li>
+							<li><span class="posttitle">Choose a Club
+							</span><p> New Club theme has been introduced.</p></li>
+				</div>
+				<div class="foot col-2">
+					<h6>Our Departments </h6>
+					<ul class="department">
+						<li><a href="mailto:support@atomicdudes.com" class="support"><i class="fa fa-ticket" aria-hidden="true"></i>support@atomicdudes.com</a></li>
+						<li><a href="mailto:events@atomicdudes.com" class="shop"><i class="fa fa-shopping-bag" aria-hidden="true"></i>shop@atomicdudes.com</a></li>
+					</ul>
+				</div>
+				<div class="foot col-3">
+					<h6>Navbar</h6>
+					<ul class="footnav">
+						<li><a href="#" class="link"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>home</a></li>
+						<li><a href="#" class="link"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>News</a></li>
+						<li><a href="#" class="link"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Forum</a></li>
+						<li><a href="#" class="link"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Tournament</a></li>
+						<li><a href="#" class="link"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Shop</a></li>
+						<li><a href="#" class="link"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Contact us</a></li>
+			</div>
+			<!--Footer Section End-->
+		</div>
+		<!--Page Wrapper End-->
+
+	<!-- custom plugin js start here -->
+	<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+</script>
+	<!-- custom plugin js End here -->
+</body>
+
+</html>
